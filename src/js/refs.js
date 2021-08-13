00a8e272273b432bdf2d064322fc2e68bd2a2cc4
Oleg Bloschinskyi <carrentkharkov@gmail.com>
2021-08-14 01:03:33 +0300

    14.08.21

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-    galleryList: document.querySelector('.js-gallery'),
-    lightBox: document.querySelector('.js-lightbox'),
-    lightboxImage: document.querySelector('.lightbox__image')
-}
